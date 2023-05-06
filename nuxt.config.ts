ee71236b598801a203f70bbccfcf24ae0b1c181b
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2023-05-06 20:03:56 +0000

    chore: add meta info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+
   app: {
     head: {
-      title: 'Pokemon Index'
-    },
+      title: "Pokemon Index",
+      meta: [
+        // facebook meta tags
+        { property: "og:url", content: "https://pokebook.ammielyawson.com" },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "Pokemon Index" },
+        {
+          property: "og:description",
+          content: "Largest Pokémon index with information about every Pokemon you can think of."
+        },
+        // twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        { property: "twitter:domain", content: "pokebook.ammielyawson.com" },
+        { name: "twitter:title", content: "Pokemon Index" },
+        {
+          name: "twitter:description",
+          content: "Largest Pokémon index with information about every Pokemon you can think of."
+        },
+        { name: "twitter:image", content: "https://pokebook.ammielyawson.com/logo-illustration.png" }
+      ]
+    }
+    ,
     pageTransition: {
-      name: 'slide-up-in',
-      mode: 'out-in'
+      name: "slide-up-in",
+      mode: "out-in"
     }
   },
 
   ssr: false,
 
-  css: ['assets/fonts/ClashDisplay/stylesheet.css', 'assets/fonts/GeneralSans/stylesheet.css'],
+  css: ["assets/fonts/ClashDisplay/stylesheet.css", "assets/fonts/GeneralSans/stylesheet.css"],
 
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui'],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-headlessui"],
 
   tailwind: {
-    cssPath: '~/assets/styles/tailwind.css',
-    configPath: 'tailwind.config.js'
+    cssPath: "~/assets/styles/tailwind.css",
+    configPath: "tailwind.config.js"
   },
 
   headlessui: {
-    prefix: 'Headless'
+    prefix: "Headless"
   }
 
-})
+});
